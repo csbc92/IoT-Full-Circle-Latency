751daa687a98188a162d3d554ccae2bf97e127f6
Jonas Vedsted Sørensen <jonso16@student.sdu.dk>
2020-03-04 16:11:40 +0100

    Update led.py

--- src/led.py
+++ src/led.py
@@ -8,22 +8,6 @@ pycom.heartbeat(False)
 uart = UART(0)                               # init with given bus
 uart.init(115200, bits=8, parity=None, stop=1) # init with given parameters:  Baudrate=9600
 
-
-## Dummy init: check LEDs are working
-for green in range(0, 255):
-    turn_on_led(rgbToInt(255, green, 0))
-for red in reversed(range(0, 255)):
-    turn_on_led(rgbToInt(red, 255, 0))
-for blue in range(0, 255):
-    turn_on_led(rgbToInt(0, 255, blue))
-for green in reversed(range(0, 255)):
-    turn_on_led(rgbToInt(0, green, 255))
-for red in range(0, 255):
-    turn_on_led(rgbToInt(red, 0, 255))
-for blue in reversed(range(0, 255)):
-    turn_on_led(rgbToInt(255, 0, blue))
-
-
 while True:
     # Receive serial communication
     myBytes = uart.read(1) # Read the received bytes into the byte array
